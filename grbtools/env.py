0b108e9f4e6faf5a491676ebee67a23714f427bb
Fahrettin Ay <ay.fahrettin@gmail.com>
2023-08-27 04:13:08 +0300

    global logger added

--- grbtools/env.py
+++ grbtools/env.py
@@ -1,4 +1,6 @@
 import os
+import coloredlogs
+import logging 
 
 # get current directory of the package
 DIR_PACKAGE = os.path.dirname(os.path.realpath(__file__))
@@ -17,3 +19,44 @@ DIR_FIGURES = os.path.join("..", "figures")
 
 # path of the models
 DIR_MODELS = os.path.join("..", "models")
+
+
+
+# default log level
+_LOG_LEVEL = "DEBUG"
+
+# log formats
+_LOG_FORMAT = (
+    "[%(levelname)-.1s]: %(message)s"
+)
+
+# the logger map;
+# keys are name of the loggers, values are the logger objects
+_loggers = dict()
+
+# get logger
+def get_logger():
+    # return if logger is already created
+    if 'root' in _loggers:
+        return _loggers['root']
+
+    # get root logger
+    logger = logging.getLogger('root')
+    # create stream handler
+    stream_handler = logging.StreamHandler()
+    # set log level
+    stream_handler.setLevel(_LOG_LEVEL)
+    # create formatter
+    formatter = coloredlogs.ColoredFormatter(_LOG_FORMAT)
+    # add formatter
+    stream_handler.setFormatter(formatter)
+    # add handler to the logger
+    logger.addHandler(stream_handler)
+
+    # set log level 
+    logger.setLevel(_LOG_LEVEL)
+    
+    # set root logger
+    _loggers['root'] = logger
+    
+    return logger
